494e1366f994dcbf1a23911048b02fa3e6587d3d
ashteya <ashteya@gmail.com>
2016-07-04 19:04:32 +0100

    Set change detection to OnPush

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";  
+import {Component, ChangeDetectionStrategy} from "@angular/core";  
 import {Modal, NavController} from 'ionic-angular';  
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs/rx';
@@ -8,7 +8,8 @@ import {Birthday} from '../../models/birthday';
 import {DetailsPage} from '../details/details';  
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage {  
     public birthdays: Observable<Birthday[]>;
